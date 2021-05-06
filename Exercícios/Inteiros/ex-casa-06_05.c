225b291a435083ab5beea35142b8cbebee04ffdd
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-06 20:07:40 -0300

    06 de maio de 2021

--- Exercícios/Inteiros/ex-casa-06_05.c
+++ Exercícios/Inteiros/ex-casa-06_05.c
@@ -1,1 +1,15 @@
+/*
+    Dado um natural n > 0, verificar se n possui dois digitos consecutivos iguais
+    exemplos: 1230 ,32527, 1001 nao possuem dígitos consecutivos iguais,
+    mas 4001 e 11 possuem.
+*/
+#include <stdio.h>
 
+int main() {
+    int n,      /*numero a ser analisado*/
+        dig_one,/**/
+        dig_two;/**/
+
+    scanf("%d ", &n);
+
+}
--- Exercícios/Inteiros/ex_sala_06_05.c
+++ Exercícios/Inteiros/ex_sala_06_05.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #define TRUE 0
 #define FALSE 1
-/*Diz se uma sequencia numerica eh ordenada*/
+/*Diz quantos alunos ficaram abaixo da media da turma*/
 int main() {
-    int first_num, second_num, i = 0, cresc = 0;
-    scanf("%d ", &first_num);
+    int *nota;
+    int n, total = 0, media, reprovados = 0;
 
-    while (cresc) {
-        scanf("%d ", second_num);
-        if (first_num > second_num) {
-            cresc = 1;
+    /*registra as notas em um array dinamico*/
+    scanf("%d ", &n);
+    notas = malloc (n * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        scanf("%d ", &nota[i]);
+        total += nota[i];
+    }
+    media = total / n;
+    /*verifica se a nota do aluno i foi menor que a media*/
+    for ( int j = 0; j < n; j++) {
+        if (nota[j] < media) {
+            reprovados++;
         }
-        else {
-            scanf("%d ", &first_num);
-        }
     }
-    if (cresc = 0) {
-        printf("A sequencia eh crescente\n");
-    }
-    else {
-        printf("A sequencia NAO eh crescente\n");
-    }
+    printf("A media é igual a %d", )
+    printf("O numero de alunos reprovados é: %d\n", reprovados);
+    free (nota)
     return 0;
 }
