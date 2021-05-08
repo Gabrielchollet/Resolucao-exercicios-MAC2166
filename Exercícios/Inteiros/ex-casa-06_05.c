1c9de4764683fb73de35ec84bfeec2acc642c472
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-08 02:01:19 -0300

    Update ex-casa-06_05.c

--- Exercícios/Inteiros/ex-casa-06_05.c
+++ Exercícios/Inteiros/ex-casa-06_05.c
@@ -8,8 +8,31 @@
 int main() {
     int n,      /*numero a ser analisado*/
         dig_one,/**/
-        dig_two;/**/
+        dig_two,/**/
+        i,      /*contador do numero de algarismos*/
+        copy,   /*guarda uma copia de n*/
+        *dig;    /*array dos i digitos de n*/
 
     scanf("%d ", &n);
 
+    /*conta o numero de algarismos n*/
+    /*e os guarda em um array*/
+    n = copy;
+    while (copy > 0) {
+        copy /= 10;
+        i++;
+    }
+    dig = malloc (i * sizeof (int));
+
+    for (int j = 0; j < i; j++) {
+        n % 10 = dig[j];
+        n /= 10;
+    }
+
+    for (int k = 0; k < i; k++) {
+
+    }
+
+    free (dig);
+    return 0;
 }
