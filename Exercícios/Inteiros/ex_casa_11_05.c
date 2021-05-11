6e6d6d398db95712441958c3e8aff4b1c20d77c7
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-11 11:37:01 -0300

    Update ex_casa_11_05.c

--- Exercícios/Inteiros/ex_casa_11_05.c
+++ Exercícios/Inteiros/ex_casa_11_05.c
@@ -1,1 +1,6 @@
-
+/*
+    Dado um inteiro positivo n, determine a sua decomposicao em
+    fatores primos calculando tambem a multipliciddade de cada fator.
+    Exemplo: para a entrada 120
+        a saída desejada eh 120 = 2^3 * 3^1 * 5^1
+*/
