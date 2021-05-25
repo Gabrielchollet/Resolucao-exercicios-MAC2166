b4f99d3208802d21679efd48f9b722323d39b898
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-25 20:33:10 -0300

    dia 25 de maio de 2021

--- Exercícios/Exercício Programa/EP02.c
+++ Exercícios/Exercício Programa/EP02.c
@@ -6,11 +6,40 @@ void PrimeiraParte( int n, int k, int N, int m, int a, int c, int r_0 );
 int main() {
     int n, k, N, m, a, c, r_0;
     scanf("%d %d %d %d %d %d %d ", &n, &k, &N, &m, &a, &c, &r_0);
-    PrimeiraParte(n, k, N, m, a, c, r_0);
+    PrimeiraParte( n, k, N, m, a, c, r_0 );
+    return 0;
 }
 
 void PrimeiraParte( int n, int k, int N, int m, int a, int c, int r_0 ) {
-    printf("Recorrencia r <-- ( r x 5 + 3 ) mod 8 a partir da semente 7.\n");
-    printf("As 5 sequencias de 3 sorteios (com repeticao) em [ 1 .. 8 ] e seus diametros:\n");
-    return;
+    printf("Recorrencia r <-- ( r x %d + %d ) mod %d a partir da semente %d.\n", a, c, m, r_0);
+    printf("As %d sequencias de %d sorteios (com repeticao) em [ 1 .. %d ] e seus diametros:\n", N, k, n);
+
+    int rn = r_0, /* termo n da recorrencia linear */
+        rn_1,     /* termo n + 1 da recorrencia linear */
+        f_r,      /* valor da funcao mapeadora */
+        diam,     /* diametro dos sorteados */
+        fmin,
+        fmax;
+
+    for (int i = 0; i < N; i++) {
+            fmin = n;
+            fmax = 1;
+            for (int j = 0; j < k; j++) {
+                rn_1 = (a * rn + c) % m;
+                f_r = rn_1*n/m + 1;
+
+                if(f_r > fmax) {
+                    fmax = f_r;
+                }
+                if(f_r < fmin) {
+                    fmin = f_r;
+                }
+                diam = 1 + fmax - fmin;
+
+                rn = rn_1;
+
+                printf("%5d", f_r);
+            }
+            printf("  -> %5d\n", diam);
+    }
 }
